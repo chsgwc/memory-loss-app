ef3dc66419dccc8cc3c7e1932296f9ba8e30ee94
Nadine <chsgirlswhocodeclub@gmail.com>
2019-09-23 19:51:14 -0400

    can add unlimited contacts now, except placement is very off

--- MemoryLossApp/app/src/main/java/com/example/memorylossapp/MakeAccount.java
+++ MemoryLossApp/app/src/main/java/com/example/memorylossapp/MakeAccount.java
@@ -1,22 +1,30 @@
 package com.example.memorylossapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.ActionBar;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MakeAccount extends AppCompatActivity {
     DatabaseHelper myDB;
     EditText editName, editUsername, editPassword, editEmail;
-    Button submitButton;
+    Button submitButton, addContactButton;
+    ConstraintLayout constraintLayout; //could be a linear layout if easier!!! (use this link:
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_account);
+
+        constraintLayout = findViewById(R.id.ogConstraintLayout);
 
         //make instance of class: DatabaseHelper, calls constructor (create DB + table)!
         myDB = new DatabaseHelper(this);
@@ -26,10 +34,28 @@ public class MakeAccount extends AppCompatActivity {
         editPassword = (EditText) findViewById(R.id.passwordText);
         editEmail = (EditText) findViewById(R.id.emailEditText);
         submitButton = (Button) findViewById(R.id.submitBtn);
+        addContactButton = (Button) findViewById(R.id.addContactBtn);
+
+        //generate contacts on the screen
+        final Context context = getApplicationContext();
+        addContactButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //dynamically add buttons
+                        TextView textView = new TextView(context);
+                        textView.setText("made in JAVA CODE!!!!! BUT I CANT POSITION IT CORRECTLY");
+                        ConstraintLayout.LayoutParams textViewLayoutParams = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+                        textView.setLayoutParams(textViewLayoutParams);
+                        constraintLayout.addView(textView);
+                    }
+                }
+        );
 
         AddData(); //method will be called whenever submit Button clicked!
     }
 
+    //storing data in database
     public void AddData() {
         submitButton.setOnClickListener(
                 new View.OnClickListener() {
@@ -44,4 +70,6 @@ public class MakeAccount extends AppCompatActivity {
                 }
         );
     }
+
+
 }
